e02a2757bef48f25eada1548cab17b96f0d1f11b
Mohsin Tabba Ovais <ahme.mohsin@gmail.com>
2024-08-02 00:02:57 +0500

    changed game css

--- app/components/Grid.jsx
+++ app/components/Grid.jsx
@@ -2,8 +2,10 @@ import Square from './Square';
 
 const Grid = ({ gameState, onSquareClick, resetGame }) => {
   return (
-    <div className="bg-gray-800 w-full min-h-screen flex flex-col items-center justify-center text-white text-[calc(10px+2vmin)]">
-      <h1 className="text-center mb-10">TIC TAC TOE GAME</h1>
+    <div className="bg-lime-500 w-full min-h-screen flex flex-col items-center justify-center text-white text-[calc(10px+2vmin)]">
+      <h1 className="text-zinc-900 text-center mb-10 text-[18px]">
+        TIC TAC TOE GAME
+      </h1>
       <div className="flex flex-col">
         {[0, 1, 2].map((i) => (
           <div className="flex flex-row" key={i}>
@@ -11,9 +13,9 @@ const Grid = ({ gameState, onSquareClick, resetGame }) => {
               <Square
                 key={i * 3 + j}
                 index={i * 3 + j}
-                className={`text-[64px] text-center text-[#1CDBF5] font-extrabold p-[30px] ${
-                  j < 2 ? 'border-r-[15px] border-r-[#1CDBF5]' : ''
-                } ${i < 2 ? 'border-b-[15px] border-b-[#1CDBF5]' : ''}`}
+                className={`text-[64px] text-center text-[rgb(255,217,0)] font-extrabold p-[30px] ${
+                  j < 2 ? 'border-r-[15px] border-r-[rgb(255,217,0)]' : ''
+                } ${i < 2 ? 'border-b-[15px] border-b-[rgb(255,217,0)]' : ''}`}
                 state={gameState.board[i * 3 + j]}
                 onClick={() => onSquareClick(i * 3 + j)}
               />
@@ -22,7 +24,7 @@ const Grid = ({ gameState, onSquareClick, resetGame }) => {
         ))}
       </div>
       <br />
-      <div className="text-white text-center text-[24px] mb-3 gap-8">
+      <div className="text-zinc-900 text-center text-[18px] mb-3 gap-8">
         <p>
           {gameState.playerX} vs {gameState.playerO}
         </p>
@@ -33,9 +35,9 @@ const Grid = ({ gameState, onSquareClick, resetGame }) => {
       <br />
       <button
         onClick={resetGame}
-        className="flex items-center justify-center bg-gradient-to-br from-[#00DDEB] via-[#5B42F3] to-[#AF40FF] border-0 rounded-lg shadow-[0_15px_30px_-5px_rgba(151,65,252,0.2)] text-white text-[20px] font-[Phantomsans,sans-serif] p-[3px] box-border cursor-pointer mt-[10px] select-none touch-manipulation whitespace-nowrap max-w-full min-w-[100px] transition-all duration-300 hover:brightness-100 active:outline-none"
+        className="flex items-center justify-center bg-gradient-to-br from-[#ff1818] via-[#02ff17] to-[#fffb03] border-0 rounded-lg shadow-[0_15px_30px_-5px_rgba(151,65,252,0.2)] text-white text-[20px] font-[Phantomsans,sans-serif] p-[3px] box-border cursor-pointer mt-[10px] select-none touch-manipulation whitespace-nowrap max-w-full min-w-[100px] transition-all duration-300 hover:brightness-100 active:outline-none"
       >
-        <span className="bg-[#282c34] px-[20px] py-[12px] rounded-lg w-full h-full transition-colors duration-300 hover:bg-transparent hover:text-black">
+        <span className="bg-[#000000] px-[15px] py-[7px] rounded-lg w-full h-full transition-colors duration-300 hover:bg-transparent hover:text-black">
           Start Again
         </span>
       </button>
@@ -44,90 +46,3 @@ const Grid = ({ gameState, onSquareClick, resetGame }) => {
 };
 
 export default Grid;
-
-//--------------------------------------
-// import Square from './Square';
-
-// const Grid = ({
-//   gameState,
-//   onSquareClick,
-//   setGameState,
-//   initialState,
-//   scoreX,
-//   scoreO,
-//   playerX,
-//   playerO,
-// }) => {
-//   const calculateWinningLine = () => {
-//     const lines = [
-//       [0, 1, 2],
-//       [3, 4, 5],
-//       [6, 7, 8],
-//       [0, 3, 6],
-//       [1, 4, 7],
-//       [2, 5, 8],
-//       [0, 4, 8],
-//       [2, 4, 6],
-//     ];
-//     for (let i = 0; i < lines.length; i++) {
-//       const [a, b, c] = lines[i];
-//       if (
-//         gameState[a] &&
-//         gameState[a] === gameState[b] &&
-//         gameState[a] === gameState[c]
-//       ) {
-//         return lines[i];
-//       }
-//     }
-//     return null;
-//   };
-
-//   const winningLine = calculateWinningLine();
-
-//   return (
-//     <div className="bg-gray-800 w-full min-h-screen flex flex-col items-center justify-center text-white text-[calc(10px+2vmin)]">
-//       <h1 className="text-center mb-10">TIC TAC TOE GAME</h1>
-//       <div className="flex flex-col">
-//         {[0, 1, 2].map((i) => (
-//           <div className="flex flex-row" key={i}>
-//             {[0, 1, 2].map((j) => (
-//               <Square
-//                 key={i * 3 + j}
-//                 index={i * 3 + j}
-//                 className={`text-[64px] text-center text-[#1CDBF5] font-extrabold p-[30px] ${
-//                   j < 2 ? 'border-r-[15px] border-r-[#1CDBF5]' : ''
-//                 } ${i < 2 ? 'border-b-[15px] border-b-[#1CDBF5]' : ''} ${
-//                   winningLine?.includes(i * 3 + j)
-//                     ? 'border-4 border-yellow-500'
-//                     : ''
-//                 }`}
-//                 state={gameState[i * 3 + j]}
-//                 onClick={() => onSquareClick(i * 3 + j)}
-//               />
-//             ))}
-//           </div>
-//         ))}
-//       </div>
-//       <br />
-//       <div className="text-white text-center text-[24px] mb-3 gap-8">
-//         <p>
-//           {playerX} vs {playerO}
-//         </p>
-//         <p>
-//           Score X : {scoreX} | Score O : {scoreO}
-//         </p>
-//       </div>
-//       <br />
-//       <button
-//         onClick={() => setGameState(initialState)}
-//         className="flex items-center justify-center bg-gradient-to-br from-[#00DDEB] via-[#5B42F3] to-[#AF40FF] border-0 rounded-lg shadow-[0_15px_30px_-5px_rgba(151,65,252,0.2)] text-white text-[20px] font-[Phantomsans,sans-serif] p-[3px] box-border cursor-pointer mt-[10px] select-none touch-manipulation whitespace-nowrap max-w-full min-w-[100px] transition-all duration-300 hover:brightness-100 active:outline-none"
-//       >
-//         <span className="bg-[#282c34] px-[20px] py-[12px] rounded-lg w-full h-full transition-colors duration-300 hover:bg-transparent hover:text-black">
-//           Start Again
-//         </span>
-//       </button>
-//     </div>
-//   );
-// };
-
-// export default Grid;
